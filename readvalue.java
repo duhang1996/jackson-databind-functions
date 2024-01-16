0c6a8137fda0f9e2758a7058a3f24d89c8825b2a
duhang1996 <57578132+duhang1996@users.noreply.github.com>
2024-01-16 18:14:27 +0800

    Update readvalue.java

--- readvalue.java
+++ readvalue.java
@@ -2,17 +2,15 @@
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class TestCase {
-    
-    ObjectMapper mapper = new ObjectMapper();    
-    
+        
     private String jackson() throws Exception {
         String s = mapper.writeValueAsString(book);
         Long startTime;
         // Jackson
         startTime = System.currentTimeMillis();
         for (int i = 0; i < LOOP_COUNT; i++) {
-            book_jackson = mapper.readValue(s, EpubViewBook.class);
-            //s = mapper.writeValueAsString(epubViewBook);
+            book_jackson = com.fasterxml.jackson.databind.ObjectMapper.readValue(s, EpubViewBook.class);
+            //s = com.fasterxml.jackson.databind.ObjectMapper.writeValueAsString(epubViewBook);
         }
         System.out.println("Jackson:" + (System.currentTimeMillis() - startTime) + ", " + s.length());
         System.out.println(s);
