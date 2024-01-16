3457c65203a9e5ebf4fa93ff4e3a6e8c780b2ea6
duhang1996 <57578132+duhang1996@users.noreply.github.com>
2024-01-16 18:21:25 +0800

    Update fuctions.java

--- fuctions.java
+++ fuctions.java
@@ -3,6 +3,6 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 public class TestCase {
         
     private void test() throws Exception {
-        com.fasterxml.jackson.databind.ObjectMapper.readValue;
+        com.fasterxml.jackson.databind.ObjectMapper.readValue();
     }
 }
