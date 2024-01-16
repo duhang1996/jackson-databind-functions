110e70f7bba35153f9d23786cd322d10e049e2b3
duhang1996 <57578132+duhang1996@users.noreply.github.com>
2024-01-16 18:31:16 +0800

    Update fuctions.java

--- fuctions.java
+++ fuctions.java
@@ -4,5 +4,8 @@ public class TestCase {
         
     private void test() throws Exception {
         com.fasterxml.jackson.databind.ObjectMapper.readValue();
+        com.fasterxml.jackson.databind.deser.std.StdDeserializer();
+        com.fasterxml.jackson.databind.DeserializationConfig.useRootWrapping();
+        com.fasterxml.jackson.databind.DeserializationConfig.withFeatures();
     }
 }
