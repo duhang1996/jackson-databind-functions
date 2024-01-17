af340db49ba2f532cfcad0e98fb5f456bff5180c
duhang1996 <57578132+duhang1996@users.noreply.github.com>
2024-01-17 10:45:34 +0800

    Create callers.java

---
+++ callers.java
@@ -0,0 +1,9 @@
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class TestCase {
+        
+    private void test() throws Exception {
+           com.fasterxml.jackson.databind.ObjectMapper._readValue();
+           com.fasterxml.jackson.databind.ObjectMapper.readValues();
+    }
+}
