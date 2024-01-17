ff5a91ce9f977cd7d541fcd01c115e0174acc004
duhang1996 <57578132+duhang1996@users.noreply.github.com>
2024-01-17 11:14:00 +0800

    Update fuctions.java

--- fuctions.java
+++ fuctions.java
@@ -8,21 +8,8 @@ import junit.framework.TestCase;
 public class JacksonTest extends TestCase {
 
     public void test_0() throws Exception {
-        ObjectMapper mapper = new ObjectMapper();
-        mapper.configure(Feature.ALLOW_UNQUOTED_FIELD_NAMES, true);
+        com.fasterxml.jackson.databind.ObjectMapper.mapper.configure(Feature.ALLOW_UNQUOTED_FIELD_NAMES, true);
 
-        mapper.readValue("{a:3}", Map.class);
-    }
-    
-    public void test_1() throws Exception {
-        Model model = new Model();
-        model.id = 1001;
-        ObjectMapper mapper = new ObjectMapper();
-        String text = mapper.writeValueAsString(model);
-        System.out.println(text);
-    }
-    
-    public static class Model {
-        public int id;
+        com.fasterxml.jackson.databind.ObjectMapper.mapper.readValue("{a:3}", Map.class);
     }
 }
