5b2d408c5ca8b945ef5ccbc99afa8336c939dc5d
duhang1996 <57578132+duhang1996@users.noreply.github.com>
2024-01-17 11:10:58 +0800

    Update fuctions.java

--- fuctions.java
+++ fuctions.java
@@ -1,8 +1,28 @@
+import java.util.Map;
+
+import com.fasterxml.jackson.core.JsonParser.Feature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
-public class TestCase {
-        
-    private void test() throws Exception {
-           com.fasterxml.jackson.databind.ObjectMapper.readValue("{a:3}", null);
+import junit.framework.TestCase;
+
+public class JacksonTest extends TestCase {
+
+    public void test_0() throws Exception {
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.configure(Feature.ALLOW_UNQUOTED_FIELD_NAMES, true);
+
+        mapper.readValue("{a:3}", Map.class);
+    }
+    
+    public void test_1() throws Exception {
+        Model model = new Model();
+        model.id = 1001;
+        ObjectMapper mapper = new ObjectMapper();
+        String text = mapper.writeValueAsString(model);
+        System.out.println(text);
+    }
+    
+    public static class Model {
+        public int id;
     }
 }
