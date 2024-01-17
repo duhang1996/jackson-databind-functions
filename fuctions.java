d6d1fee33c7e60ea7bb55e1ff780f685c0d4fe50
duhang1996 <57578132+duhang1996@users.noreply.github.com>
2024-01-17 11:23:25 +0800

    Update fuctions.java

--- fuctions.java
+++ fuctions.java
@@ -1,14 +1,10 @@
-import java.util.Map;
-
-import com.fasterxml.jackson.core.JsonParser.Feature;
-import com.fasterxml.jackson.databind.ObjectMapper;
 
 import junit.framework.TestCase;
 
 public class JacksonTest extends TestCase {
 
     public void test_0() throws Exception {
-        ObjectMapper s = new ObjectMapper();
+        com.fasterxml.jackson.databind.ObjectMapper s = new com.fasterxml.jackson.databind.ObjectMapper();
         s.readValue();
     }
 }
