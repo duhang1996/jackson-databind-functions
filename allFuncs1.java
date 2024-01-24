70e9c62d77582eca5c7895ec251aae8b5add2689
duhang1996 <57578132+duhang1996@users.noreply.github.com>
2024-01-24 18:26:24 +0800

    Update allFuncs1.java

--- allFuncs1.java
+++ allFuncs1.java
@@ -1,7 +1,7 @@
 import com.fasterxml.jackson.databind.ObjectMapper;
 public class allFuncs1 {
         
-    private void test() throws Exception {
+    private void objectMapper() throws Exception {
            
 ObjectMapper clazz20327 = new ObjectMapper();
             clazz20327.enableDefaultTyping(null);
