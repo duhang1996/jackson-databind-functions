5d95a650eb4ccebba72fa7c008a6b981ae5fa35d
duhang1996 <57578132+duhang1996@users.noreply.github.com>
2024-01-24 18:37:35 +0800

    Update allFuncs1.java

--- allFuncs1.java
+++ allFuncs1.java
@@ -6,7 +6,12 @@ import java.time.Duration;
 public class allFuncs1{
 
     private void objectMapper() {
-        ObjectMapper objectMapper = new ObjectMapper();
-        objectMapper.enableDefaultTyping(null);
+        ObjectMapper clazz20327 = new ObjectMapper();
+            clazz20327.enableDefaultTyping(null);
+clazz20327.writerWithView(null);
+clazz20327.writerWithType(null);
+clazz20327.writerWithDefaultPrettyPrinter(null);
+clazz20327.writerFor(null);
+clazz20327.writer(null);
     }
 }
