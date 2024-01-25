b36bb4bea0ee2d54a3c239c857695410c9837f68
duhang1996 <57578132+duhang1996@users.noreply.github.com>
2024-01-25 15:57:18 +0800

    Update callers.java

--- callers.java
+++ callers.java
@@ -3,7 +3,12 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 public class callers {
         
     private void test() throws Exception {
-           com.fasterxml.jackson.databind.ObjectMapper._readValue();
            com.fasterxml.jackson.databind.ObjectMapper.readValues();
+    }
+
+    class innerClass {
+            private void innerTest {
+                   com.fasterxml.jackson.databind.ObjectMapper._readValue();
+            }
     }
 }
